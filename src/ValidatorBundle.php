db1d3aa7ee2bcafd61339f3f95e2e25b57e789c2
Fabian Fröhlich <fafr@uni-bremen.de>
2020-04-25 13:06:49 +0200

    Bundle place fix

---
+++ src/ValidatorBundle.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Symfony Validator
+ *
+ * Type sensitive validating Software for Symfony Applications.
+ *
+ * Copyright (c) 2020 Fabian Fröhlich <mail@f-froehlich.de> https://f-froehlich.de
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For all license terms see README.md and LICENSE Files in root directory of this Project.
+ *
+ */
+
+namespace FabianFroehlich\Validator;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+/**
+ * Class ValidatorBundle
+ *
+ * @author  Fabian Fröhlich <mail@f-froehlich.de>
+ * @package FabianFroehlich\Validator
+ */
+class ValidatorBundle
+    extends Bundle {
+
+}
